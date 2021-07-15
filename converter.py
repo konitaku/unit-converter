aadfc1a12f5f8045298abde0d9d9268fd0394ee5
Takuma Konishi <takm2357msc@icloud.com>
2021-07-15 16:13:39 +0900

    Initial Commit

---
+++ converter.py
@@ -0,0 +1,56 @@
+from decimal import Decimal, ROUND_HALF_UP
+
+
+def convert(num: int, init_unit: str, change_unit: str) -> int:
+    result = 0
+    if init_unit == "m":
+        cv_rate = {
+            "m": 1,
+            "mile": 1/1609,
+            "yard": 1.09,
+            "feet": 3.281,
+            "inch": 39.370
+        }
+        result = num * cv_rate[change_unit]
+        result = Decimal(str(result)).quantize(Decimal('0.0001'), rounding=ROUND_HALF_UP)
+    if init_unit == "mile":
+        cv_rate = {
+            "m": 1609,
+            "mile": 1,
+            "yard": 1760,
+            "feet": 5280,
+            "inch": 63360
+        }
+        result = num * cv_rate[change_unit]
+        result = Decimal(str(result)).quantize(Decimal('0.1'), rounding=ROUND_HALF_UP)
+    if init_unit == "yard":
+        cv_rate = {
+            "m": 1/1.094,
+            "mile": 1/1760,
+            "yard": 1,
+            "feet": 3,
+            "inch": 36
+        }
+        result = num * cv_rate[change_unit]
+        result = Decimal(str(result)).quantize(Decimal('0.0000001'), rounding=ROUND_HALF_UP)
+    if init_unit == "feet":
+        cv_rate = {
+            "m": 1/3.281,
+            "mile": 1/5280,
+            "yard": 1/3,
+            "feet": 1,
+            "inch": 12
+        }
+        result = num * cv_rate[change_unit]
+        result = Decimal(str(result)).quantize(Decimal('0.0000000001'), rounding=ROUND_HALF_UP)
+    if init_unit == "inch":
+        cv_rate = {
+            "m": 1/39.37,
+            "mile": 1/63360,
+            "yard": 1/36,
+            "feet": 1/12,
+            "inch": 1
+        }
+        result = num * cv_rate[change_unit]
+        result = Decimal(str(result)).quantize(Decimal('0.0000000001'), rounding=ROUND_HALF_UP)
+    return result
---
+++ server.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField
+from wtforms.validators import DataRequired
+from converter import convert
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "\xbaF\xf0by\xb7\xf5Q\x91\xfb\xd2\x824fN\xce\xaa\x1d\xb4F\xady\x12\xaf"
+
+
+class ConvertForm(FlaskForm):
+    number = StringField(validators=[DataRequired()], render_kw={"placeholder": "1"})
+    unit_select1 = SelectField(
+        choices=[("m", "メートル"), ("mile", "マイル"), ("yard", "ヤード"), ("feet", "フィート"), ("inch", "インチ")],
+        validators=[DataRequired()]
+    )
+    unit_select2 = SelectField(
+        choices=[("m", "メートル"), ("mile", "マイル"), ("yard", "ヤード"), ("feet", "フィート"), ("inch", "インチ")],
+        validators=[DataRequired()]
+    )
+
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    form = ConvertForm()
+    result = 0
+    if form.validate_on_submit():
+        result = convert(int(form.number.data), form.unit_select1.data, form.unit_select2.data)
+
+        form = ConvertForm(
+            number=form.number.data,
+            unit_select1=form.unit_select1.data,
+            unit_select2=form.unit_select2.data
+        )
+    return render_template("index.html", form=form, pagename="home", result=result)
+
+
+@app.route("/other")
+def other():
+    return render_template("other.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
